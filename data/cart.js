9951d4a60bba782c32dd1785f8303a10d4d063da
Saugat Rana <ranasaugat9@gmail.com>
2025-05-14 08:15:05 -0400

    Created automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,33 @@
-export let cart= JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart){
 
-  cart= [{
+loadFromStorage();
 
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2,
-    deliveryOptionId:'1'
-  },
-  {
+export function loadFromStorage(){
+  cart= JSON.parse(localStorage.getItem('cart'));
+
+  if(!cart){
   
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1,
-     deliveryOptionId:'2'
+    cart= [{
   
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity:2,
+      deliveryOptionId:'1'
+    },
+    {
+    
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity:1,
+       deliveryOptionId:'2'
+    
+    
+    }];
+    
+  }
   
-  }];
-  
-}
+
+
+};
 function saveToStorage (){
 
 
@@ -36,8 +46,20 @@ export function addToCart(productId){
   });
 
   const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-
+if (addedMessage) {
   addedMessage.classList.add('added-to-cart-visible');
+
+  if (addedMessageTimeoutId) {
+    clearTimeout(addedMessageTimeoutId);
+  }
+
+  const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
+  }, 2000);
+
+  addedMessageTimeoutId = timeoutId;
+}
+
 
   if(addedMessageTimeoutId){
     clearTimeout(addedMessageTimeoutId);
@@ -50,10 +72,9 @@ export function addToCart(productId){
   },2000);
 
   addedMessageTimeoutId=timeoutId;
-  const quantitySelector =  document.querySelector(`.js-quantity-selector-${productId}`);
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+const quantity = quantitySelector ? Number(quantitySelector.value) : 1;
 
-
-  const quantity = Number(quantitySelector.value);
 
   if(matchingItem){
     matchingItem.quantity+=quantity;
