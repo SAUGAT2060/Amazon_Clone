180a96e80303e134377572eabb47bb15c9d73441
Saugat Rana <ranasaugat9@gmail.com>
2025-05-18 01:34:01 -0400

    Learn Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,47 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 
-loadProducts(() => {
-  renderOrderSummary();
+
+Promise.all([
+
+  new Promise( (resolve) =>{
+  loadProducts(() =>{
+    resolve('value1');
+  });
+}) ,
+
+new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+
+]).then(()=>{
+
+ renderOrderSummary();
   renderPaymentSummary();
 
 });
+
+
+
+// new Promise( (resolve) =>{
+//   loadProducts(() =>{
+//     resolve('value1');
+//   });
+// }).then((value)=>{
+//   console.log(value);
+//   return new Promise((resolve)=>{
+//     loadCart(()=>{
+//       resolve();
+//     });
+//   });
+
+// }).then(()=>{
+//   renderOrderSummary();
+//   renderPaymentSummary();
+
+// });
 
