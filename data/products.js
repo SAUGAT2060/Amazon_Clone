dc2a9ea9d92bdcd4ef7d2c3ce0fed21652edd95d
Saugat Rana <ranasaugat9@gmail.com>
2025-05-18 11:23:36 -0400

    Create order using the backend

--- data/products.js
+++ data/products.js
@@ -783,7 +783,7 @@ export function loadProductsFetch(){
 
   return response.json();
   }).then((productsData)=>{
-    console.log(productsData);
+    // console.log(productsData);
     
      products = productsData.map((productDetails)=>{
       if(productDetails.type === 'clothing'){
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,8 @@ import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
+import {addOrder} from '../../data/orders.js'
+
 export function renderPaymentSummary(){
  let productPriceCents= 0;
 let shippingPriceCents = 0;
@@ -58,7 +60,8 @@ let shippingPriceCents = 0;
                   <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
                 </div>
 
-                <button class="place-order-button button-primary">
+                <button class="place-order-button button-primary 
+                js-place-order">
                   Place your order
                 </button>
       
@@ -66,4 +69,38 @@ let shippingPriceCents = 0;
  `;
  document.querySelector('.js-payment-summary')
 .innerHTML= paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click' , 
+   async () =>{
+
+    try{
+            const response = await fetch('https://supersimplebackend.dev/orders',{
+
+
+            method:'POST' ,
+            headers:{
+              'Content-Type':'application/json'
+
+
+            },
+            body:JSON.stringify({
+              cart: cart
+            })
+          });
+
+        const order = await response.json();
+        
+        addOrder(order);  
+
+    }
+    catch(error){
+
+      console.log('Unexpected Error. Try again later.');
+    }
+
+
+    window.location.href = 'orders.html';
+
+
+  });
 }
